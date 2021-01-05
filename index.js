b359aa714ba5b9b9077062c65375de67d3ab2aa3
Zastrich <zas@code200.com.br>
2021-01-05 16:30:21 -0300

    Change running script

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const scripts = require('./scripts')
-
-const [,, command, ...args] = process.argv
-
-if (typeof scripts[command] === 'function') {
-  scripts[command](args)
-} else {
-  console.log('')
-
-  if (typeof command !== 'undefined') {
-    console.log('\x1b[36m%s\x1b[32m%s\x1b[36m', 'Script ', `"${command}"`, 'not found')
-    console.log('')
-  }
+const run = async () => {
+  const scripts = require('./bin')
   
-  console.log('\x1b[36m%s', 'Available scripts:')
-  console.log('\x1b[31m%s', `${Object.keys(scripts).join('\n')}`)
+  const [,, command, ...args] = process.argv
+  
+  if (typeof scripts[command] === 'function') {
+    scripts[command](args)
+  } else {
+    console.log('')
+  
+    if (typeof command !== 'undefined') {
+      console.log('\x1b[36m%s\x1b[32m%s\x1b[36m', 'Script ', `"${command}"`, 'not found')
+      console.log('')
+    }
+    
+    console.log('\x1b[36m%s', 'Available scripts:')
+    console.log('\x1b[31m%s', `${Object.keys(scripts).join('\n')}`)
+  }
 }
+
+run()
